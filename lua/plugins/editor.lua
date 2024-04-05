138bed33670502f5e79bc82ed36b04fd903ce016
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2024-04-05 12:21:50 +0200

    feat: add gh-addressed

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -49,4 +49,17 @@ return {
     ft = { "json" },
     config = true,
   },
+
+  {
+    "dlvhdr/gh-addressed.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "folke/trouble.nvim",
+    },
+    cmd = "GhReviewComments",
+    keys = {
+      { "<leader>gr", "<cmd>GhReviewComments<cr>", desc = "GitHub Review Comments" },
+    },
+  },
 }
