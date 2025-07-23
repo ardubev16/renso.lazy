a9d1cfc194cf9e9613360f6e77ac559ba442b3c0
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2025-07-23 18:23:53 +0200

    feat: update lazy.lua, add pymple

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "120"
 vim.opt.mouse = "nv"
 vim.opt.relativenumber = false
 vim.opt.shiftwidth = 4
