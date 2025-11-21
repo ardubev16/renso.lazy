139b353a076751ca5ee00719cf4ca70dce0ec805
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2025-11-21 10:51:24 +0100

    feat: use arrows to move between tabs and windows

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,29 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+local unmap = vim.keymap.del
 
 -- quit
-vim.keymap.del("n", "<leader>qq")
+unmap("n", "<leader>qq")
+
+map("n", "<S-Right>", "<cmd>tabnext<cr>")
+map("n", "<S-Left>", "<cmd>tabprevious<cr>")
+
+-- Move to window using the <ctrl> arrow keys
+-- NOTE: LazyVim uses <ctrl> arrow keys to resize windows
+unmap("n", "<C-Left>")
+unmap("n", "<C-Down>")
+unmap("n", "<C-Up>")
+unmap("n", "<C-Right>")
+
+map("n", "<C-Left>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-Down>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-Up>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-Right>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- Move to window using the <ctrl><shift> arrow keys
+map("n", "<C-S-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
